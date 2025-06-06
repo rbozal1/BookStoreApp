c0ba5ded3b17ee3626742fd62199f61594829867
Roxhers Bozali <Roxhers.Bozali@metlabs.com>
2025-06-06 16:54:40 -0400

    Completed Setup for both Book and Author API Controller and functionality

---
+++ BookStoreApp.API/Models/Author/AuthorReadOnlyDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.API.Models.Author
+{
+    public class AuthorReadOnlyDto : BaseDto
+    {
+       
+        public string? FirstName { get; set; }
+
+      
+        public string? LastName { get; set; }
+
+       
+        public string? Bio { get; set; }
+    }
+}
--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,8 +1,15 @@
+using BookStoreApp.API.Configurations;
+using BookStoreApp.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
+builder.Services.AddDbContext<BookStoreDbContext>(options =>options.UseSqlServer(connString));
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+builder.Services.AddControllers();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,6 +28,7 @@ builder.Services.AddCors( options =>
              .AllowAnyMethod()
              .AllowAnyHeader();
         });
+});
 
 var app = builder.Build();
 
